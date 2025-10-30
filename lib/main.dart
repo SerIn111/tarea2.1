2cbdb21bc0c80148e33b4cc36959442b06bdc335
SerTroyan111 <sergio.inestroza@unah.hn>
2025-10-30 10:52:21 -0600

    GoRouter() agregado

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tarea2_1/src/widgets/mytextfield.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tarea2_1/src/views/login.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,52 +9,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple[100],
-          title: const Center(child: Text('Inicio de sesion', style: TextStyle(fontSize: 30),)),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(10),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blue[100]
-                ),
-                child: Column(
-                  children: [
-                    const Text('Ingrese su correo', style: TextStyle(fontSize: 20),),
-                    MyTextfield(
-                    type: TextInputType.emailAddress,
-                    textoejemplo: 'ejemplo@unah.hn',
-                    ),
-                  ],
-                ),
-
-              ),
-              const Text('Ingrese su contraseña', style: TextStyle(fontSize: 20),),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: MyTextfield(
-                  type: TextInputType.number,
-                  obscuretext: true,
-                  icono: Icon(Icons.remove_red_eye_rounded),
-                ),
-              ),
-            ],
+      title: 'Tarea 2.1',
+      routerConfig: GoRouter(
+        initialLocation: '/login',
+        routes: [
+          GoRoute(
+            path: '/login', 
+            name: 'login', 
+            builder: (state, context) => Login()
           ),
-        ),
-        bottomSheet: TextButton(
-          child: const Text('Registrarse', style: TextStyle(color: Colors.blue),),
-          onPressed: () {},
-        )
+        ]
       ),
     );
   }
---
+++ lib/src/views/login.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:tarea2_1/src/widgets/mytextfield.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple[100],
+          title: const Center(child: Text('Inicio de sesion', style: TextStyle(fontSize: 30),)),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue[100]
+                ),
+                child: Column(
+                  children: [
+                    const Text('Ingrese su correo', style: TextStyle(fontSize: 20),),
+                    MyTextfield(
+                    type: TextInputType.emailAddress,
+                    textoejemplo: 'ejemplo@unah.hn',
+                    ),
+                  ],
+                ),
+
+              ),
+              const Text('Ingrese su contraseña', style: TextStyle(fontSize: 20),),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MyTextfield(
+                  type: TextInputType.number,
+                  obscuretext: true,
+                  icono: Icon(Icons.remove_red_eye_rounded),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomSheet: TextButton(
+          child: const Text('Registrarse', style: TextStyle(color: Colors.blue),),
+          onPressed: () {},
+        )
+      ),
+    );
+  }
+}
